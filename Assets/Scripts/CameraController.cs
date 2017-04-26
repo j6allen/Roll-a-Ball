3a351a7c5238ba99d5e9a4ccfa34229a70e0a6c0
Jesse Allen <j6allen@gmail.com>
2017-04-25 19:22:27 -0700

    Cleaned up un-used Method

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -11,11 +11,7 @@ public class CameraController : MonoBehaviour {
         offset = transform.position - player.transform.position;
 	}
 	
-	// Update is called once per frame
-	void Update ()
-    {
-        
-	}
+
     /// <summary>
     /// runs after all other updates
     /// </summary>
